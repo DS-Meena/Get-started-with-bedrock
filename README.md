964cdb98a9ffca363ea738006c21a2882a55ace6
28-08-2000 <dharamsinghmeena2000@gmail.com>
2024-03-02 15:05:47 +0530

    updated readme file and added code for llm

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# bedrock_basic
- Get started with Bedrock on AWS
+# Getting Started with Bedrock Basics
+
+## Create a virtual enviornment
+
+First, we should create a virtual environment to keep your global enviornment clean. 
+
+**In Windows System**
+
+1. Install virtualenv using pip `pip install virtualenv`
+2. Create a virtual environment using `python -m venv my_env`
+3. To activate your virtual envionrment use `my_env\Scripts\activate`
+4. `deactivate` simply to deactivate
+
+**In Linux System**
+
+The first step is same as windows system
+1. Use `virtualenv my_env` to create a virtual environment.
+2. To start the your newly created virtual environment use `source my_env/bin/activate`.
+3. and to deactivate simply type `deactivate`
--- main.py
+++ main.py
@@ -1,4 +1,42 @@
 from langchain import hub
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import Bedrock
+from langchain.chains import LLMChain
+from langchain.agents import tool, Tool, create_react_agent, AgentExecutor
+
+@tool
+def sum_list(string_list_of_nums: str) -> int:
+    """Takes an python string containing a list of numbers, converts them to ints and sum them all together to provide a total."""
+    # firstly lets make sure we are working with int
+    string_list_of_nums = string_list_of_nums.replace("[", "")
+    string_list_of_nums = string_list_of_nums.replace("]", "")
+    string_list_of_nums = string_list_of_nums.split(", ")
+
+    # convert list of strings into list of ints
+    list_nums = list(map(int, string_list_of_nums))
+    print(list_nums)
+
+    for i in range(len(list_nums)):
+        list_nums[i] = int(list_nums[i])
+    return sum(list_nums)
+
+def llm_with_tools(question):
+    prompt = hub.pull("hwchase17/react-chat")
+
+    sum_tool = Tool(
+        name="Sum_tool",
+        func=sum_list,
+        description="useful for adding numbers",
+    )
+    tools = [sum_tool]
+
+    llm = Bedrock(
+        credentials_profile_name="default",model_id="anthropic.claude-v2", region_name="a-south-1"
+    )
+    agent = create_react_agent(llm=llm,tools=tools,prompt=prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+    answer = agent_executor.invoke({"input": f"{question}. Only use a use tool if need, otherwise respond with final answer.", "chat history": ""})
+    print(answer["output"])
 
 def plain_llm(question):
     template = """Question: {question}
@@ -10,5 +48,22 @@ def plain_llm(question):
     llm = Bedrock(
         credentials_profile_name="default",model_id="anthropic.claude-v2", region_name="a-south-1"
     )
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+    print(llm_chain.invoke(question))
 
+def main():
+    question = "Can I get a job in Amazon"
+    plain_llm(question)
 
+    # list_of_nums = [645, 6, 2, 9, 2]
+    # question = f"Sum of the following numbers: {list_of_nums}"
+
+    # plain_llm(question)
+    # expected_sum = sum(list_of_nums)
+    # print(f"\nExpected sum: {expected_sum}")
+
+    # llm_with_tools(question)
+    # expected_sum = sum(list_of_nums)
+    # print(f"\nExpected sum: {expected_sum}")
+
+main()
